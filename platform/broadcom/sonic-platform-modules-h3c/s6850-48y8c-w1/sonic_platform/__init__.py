fa304785be88630bae1ad293d4a76d3d807e5411
zhaiguangcheng10865 <zhai.guangcheng@h3c.com>
2022-11-03 21:16:57 +0800

    process reboot-cause bug

--- platform/broadcom/sonic-platform-modules-h3c/s6850-48y8c-w1/sonic_platform/__init__.py
+++ platform/broadcom/sonic-platform-modules-h3c/s6850-48y8c-w1/sonic_platform/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ["platform", "chassis", "fan", "psu", "sfp", "thermal", "component", "watchdog", "eeprom", "fan_drawer"]
 #from sonic_platform import *
+from sonic_platform import platform
